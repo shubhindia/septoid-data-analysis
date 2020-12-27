bd88bf18f5c362f479c4018da1406b54632ac550
shubhindia <shubhindia123@gmail.com>
2020-12-27 12:09:55 +0530

    clean the data and choose between buying and selling
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

---
+++ analysis.py
@@ -0,0 +1,25 @@
+import pandas as pd
+df = pd.read_csv("data.txt", index_col=False, header=None)
+usable_data= []
+for i in df.values:
+    if i[2] > "09:16" and i[2] < "15:30":
+        usable_data.append(i)
+usable_dataframe = pd.DataFrame(usable_data)
+
+for i in usable_dataframe.values:
+    j = 0
+    k = 0
+    sma14_list = []
+    sma30_list = []
+    while(j <  14):
+        sma14_list.append(i[5])
+        j +=1
+    sma14_avg = sum(sma14_list) / len(sma14_list)
+    while(k < 30):
+        sma30_list.append(i[5])
+        k +=1
+    sma30_avg = sum(sma30_list) / len(sma30_list)
+    if sma14_avg > sma30_avg:
+        print("{} {} {}".format(i[1], i[2], "BUY"))
+    else:
+        print("{} {} {}".format(i[1], i[2], "SELL"))
