c865f6d83a2c2b61a3234477b6eebd8cef657e9a
shubhindia <shubhindia123@gmail.com>
2020-12-28 10:53:28 +0530

    get stocks data
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- stock_indices.py
+++ stock_indices.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import lxml.html as lh
 url = 'https://www.investing.com/indices/major-indices'
 header = {
   "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
@@ -8,10 +9,9 @@ header = {
 }
 result = requests.get(url, headers=header)
 soup = BeautifulSoup(result.text, features="lxml")
-table = soup.find("table", attrs={"id":"QBS_2_inner"})
-headings = [th.get_text() for th in table.find("tr").find_all("th")]
 datasets = []
-for row in table.find_all("tr")[1:]:
-    dataset = list(zip(headings, (td.get_text() for td in row.find_all("td"))))
-    datasets.append(dataset)
-print(datasets)
+for row in soup.find_all('tr'):
+    for col in row.find_all('td'):
+        info = col.text
+        print(info)
+        
